e5fcc13f3f71894797d90ce8bbb3a4c9a0f59ec9
santhoshkumar2 <46369611+santhoshkumar2@users.noreply.github.com>
2019-02-03 16:07:34 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,10 +1,10 @@
-a=int(raw_input())
-b=int(raw_input())
-c=int(raw_input())
-if (a>=b) and (a>=c):
-	largest=a
-elif (b>=a) and (b>=c):
-	largest=b
+a1=int(raw_input())
+b1=int(raw_input())
+c1=int(raw_input())
+if (a1>=b1) and (a1>=c1):
+	largest=a1
+elif (b1>=a1) and (b1>=c1):
+	largest=b1
 else:
-	largest=c
+	largest=c1
 print int(largest)
