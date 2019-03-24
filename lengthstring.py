4f266f22e778059bd189673c5776a167d9a92b63
santhoshkumar2 <46369611+santhoshkumar2@users.noreply.github.com>
2019-03-24 16:40:42 +0530

    Update lengthstring.py

--- lengthstring.py
+++ lengthstring.py
@@ -1,8 +1,5 @@
-a=raw_input()
-b=raw_input()
-a=a.split()
-b=b.split()
-sum=0
-for i in range(int(a[1])):
-	sum=sum+int(b[i])
-print sum
+word=raw_input()
+count=0
+for i in word:
+	count+=1
+print count
